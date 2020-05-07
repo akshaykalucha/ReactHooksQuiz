1b596d4970c9eeeb814dada636de5588224ba5d6
akshay kalucha <akshaykalucha@gmail.com>
2020-05-07 19:32:55 +0530

    modifications in code

--- quiz/Quiz.jsx
+++ quiz/Quiz.jsx
@@ -40,21 +40,17 @@ function Quiz() {
 
     const [Allanswered, setAllAnswered] = useState(false)
     const [result, setResult] = useState(0)
+    const [userans, setuserns] = useState([])
 
     useEffect(() => {
         console.log('hi')
     }, [])
 
     const AnswerKey = [1, 1, 3, 2]
-    const [userans, setuserns] = useState([])
-    let i
-    function questionList() {
-        for(i=0; i<Questions.length; i++){
-            // const k = Questions[i]['Question']
-            // console.log(k)
-        }
-    }
+
     let Nresult = 0
+
+
     function getResult(arr1, arr2) {
         
         for(let i = 0; i < arr1.length; i++){
@@ -65,22 +61,21 @@ function Quiz() {
         setResult(Nresult)
         return Nresult
     }
-    questionList()
+
+    
     const handlechange = (e, index) => {
-        // console.log(index, 'indexxx')
-        // console.log(Questions.length, 'lemgthh')
+
         if (Questions.length === index+1){
-            // console.log('yesssss')
+
             setAllAnswered(true)
             console.log(e.currentTarget.innerText)
-            // console.group(Allanswered)
             let newArr = [...Questions]
             newArr[index].isAnswered = true; // replace e.target.value with whatever you want to change it to
             newArr[index].isNext = false;
             setQuestios(newArr);
             let optiontext = e.currentTarget.innerText
             let bkey
-            Questions.map(Question => Question.Options.map(function(option, index){
+            Questions.map(Question => Question.Options.map(function(option){
                 if(option.op1===optiontext){
                     bkey = option.b
                 }else if(option.op2 === optiontext){
@@ -97,13 +92,14 @@ function Quiz() {
             console.log(getResult(userlist, AnswerKey), 'your result is')
             return
         }
+
+
         let bkey
-        // const answer = e.currentTarget.value
         let newArr = [...Questions]; // copying the old datas array
         newArr[index].isAnswered = true; // replace e.target.value with whatever you want to change it to
         newArr[index].isNext = false;
         let optiontext = e.currentTarget.innerText
-        Questions.map(Question => Question.Options.map(function(option, index){
+        Questions.map(Question => Question.Options.map(function(option){
             if(option.op1===optiontext){
                 bkey = option.b
             }else if(option.op2 === optiontext){
@@ -113,25 +109,17 @@ function Quiz() {
             }
             return bkey
         }))
+
         newArr[index+1].isNext = true
         let userlist = [...userans]
         userlist.push(parseInt(bkey))
         setuserns(userlist)
         console.log(userlist)
         setQuestios(newArr);
-        if(bkey === e.target.value){
-            // console.log('hihii')
-            // setQuestios(prevState => ({ ...prevState, prevState.: true}));
-            // setQuestios(Questions.map(item => item.key === index+1 ? {...item, isAnswered : "true"} : item ))
-            // const k =Questions.find(x => x.key === index+1)
-            // console.log(k)
-            // k.isAnswered = "true"
-            // setQuestios(Questions.find(index).isAnswered= "true")
-        }else{
-            // console.log(e.currentTarget.innerText, bkey, e.currentTarget.value)
-        }
-        // console.log(index)
     }
+
+
+    
     if(Allanswered === false){
         return(
             Questions.map((Question, index) => Question.isAnswered === false && Question.isNext === true ? <div>
