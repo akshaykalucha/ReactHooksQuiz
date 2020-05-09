062589ec433294ef9790af868c2c49ed4573ac29
akshay kalucha <akshaykalucha@gmail.com>
2020-05-09 15:08:44 +0530

    RoutingSorted

---
+++ quiz/Index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { QuizStart } from './QuizStart'
+import Quiz from './Quiz'
+
+export class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                <Switch>
+                    <Route path="/" exact component={QuizStart} />
+                    <Route path="/quiz">
+                        <Quiz />
+                    </Route>
+                </Switch>
+                </Router>
+            </div>
+        )
+    }
+}
+
+export default Index
--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -1,35 +1,16 @@
 import React, { Component } from 'react'
-import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Quiz from './Quiz'
-import Home from './Home'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 
 export class QuizStart extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
-    }
     
     render() {
         return (
-            <Router>
-                <div> 
-                    <Link to='/quiz' color="white" clssName="is-rounded">
-                        <span>My Quiz</span>
-                    </Link>
-                    <Switch>
-                        <Route  path="/quiz" component={Quiz} />
-                        </Switch>
-                </div>
-            </Router>
+        <div>
+            <Link to="/quiz">My Quiz</Link>
+        </div>
         )
     }
 }
 
 export default QuizStart
-
-
-//Need to get packages
