f00de1e39754fe29001ff1bb87e809456c9de052
akshay kalucha <akshaykalucha@gmail.com>
2020-05-10 23:58:17 +0530

    checkbox

--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -10,7 +10,7 @@ export class QuizStart extends Component {
     
       this.state = {
         Num: '',
-        Genre: "Volvo"
+        Genre: "GK"
       }
 
       this.globalvar = {
@@ -33,13 +33,23 @@ export class QuizStart extends Component {
     render() {
         return (
         <div>
-            <label>Please enter number of questions you want</label><br/>
-            <input type="text" placeholder="..." name="Num" value={this.state.Num} onChange={e => this.setState({[e.target.name]: e.target.value})} /><br/>
+            <label>Please select level of difficulty</label><br/>
+            <div>
+            <input type="radio" id="huey" name="Easy" value="huey"checked />
+             <label for="huey">Easy</label>
+            </div>
+            <div>
+            <input type="radio" id="huey" name="Medium" value="huey" />
+             <label for="huey">Medium</label>
+            </div>            <div>
+            <input type="radio" id="huey" name="Difficult" value="huey" />
+             <label for="huey">Difficult</label>
+            </div>
             <select name="Genre" value={this.state.Genre} onChange={e => this.setState({[e.target.name]: e.target.value}) } id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
+                <option value="GK">GK</option>
+                <option value="Science">Science</option>
+                <option value="History">History</option>
+                <option value="Civics">Civics</option>
             </select>
             <button className="otpbutton">
                 <Link to={{ pathname: "/quiz", state: this.state }}>Create Quiz</Link>
