d8882e2a698e705b32d7c6f280a34423c0d940e8
akshay kalucha <akshaykalucha@gmail.com>
2020-05-12 00:52:52 +0530

    new frame

--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -40,6 +40,8 @@ export class QuizStart extends Component {
         })
     }
 
+    //CORE DATA STRUCTURE REPLICATION API CALL AND SETTING STATE
+
     crateQiuz = e => {
         this.setState({
             quizCreated: true
@@ -52,18 +54,58 @@ export class QuizStart extends Component {
         let i
         for(i=0; i<QuestionArray.length; i++){
             // for(let j = 0; i<AnswerArray.length; j++){
-            console.log(QuestionArray[i])
+            // console.log(QuestionArray[i])
             let k = {
                 Question: QuestionArray[i],
-                Options: AnswerArray[i]
+                // Options: AnswerArray[i]
             }
             Questionlist = k
                 // Questionlist["Options"] = AnswerArray[i]
             QuesArray.push(Questionlist)
-            // }
+            // }   
+        }
+        let option = []
+        let j
+        for(j=0; j<AnswerArray.length; j++){
+            let k =  AnswerArray[j]
+            // console.log(k)
+            // let j
+            for(i=0; i<k.length; i++){
+                let options = `op${i+1}`
+                let newoption = {
+                    [options]: k[i],
+                    b: i+1
+                }
+                option.push(newoption)
+            }
             
         }
+        function chunkArray(myArray, chunk_size){
+            var index = 0;
+            var arrayLength = myArray.length;
+            var tempArray = [];
+            
+            for (index = 0; index < arrayLength; index += chunk_size) {
+                let myChunk = myArray.slice(index, index+chunk_size);
+                // Do something if you want with the group
+                tempArray.push(myChunk);
+            }
+        
+            return tempArray;
+        }
+        var result = chunkArray(option, 3);
+        // console.log(result)
+        let t
+        for(t=0; t<QuesArray.length; t++){
+            // console.log(QuesArray[t], `this question is at ${t} index`)
+            QuesArray[t]["Options"] = result[t]
+            // console.log(result[t], `this result is at ${t} index`)
+        }
         console.log(QuesArray)
+        // let z 
+        // for(z=0; z<result.length; z++){
+        //     console.log(result[z], `this result is at ${z} index`)
+        // }
         // setTimeout(() => this.setState({ redirect: true }), 8000);
     }
     
