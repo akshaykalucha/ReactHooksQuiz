b9308539718d7003bb2cd184ce20eefe83502d2a
akshay kalucha <akshaykalucha@gmail.com>
2020-05-15 20:22:52 +0530

    result

--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -151,15 +151,15 @@ export class QuizStart extends Component {
         this.setStateQuiz(QuesArray)
     }
     
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            console.log(this.state, 'this state is redirecting')
-          return <Redirect to={{
-            pathname: '/quiz',
-            state: this.state.quizDic
-        }} />
-        }
-      }
+    // renderRedirect = () => {
+    //     if (this.state.redirect) {
+    //         console.log(this.state, 'this state is redirecting')
+    //       return <Redirect to={{
+    //         pathname: '/quiz',
+    //         state: this.state.quizDic
+    //     }} />
+    //     }
+    //   }
     
     render() {
         // const { history } = this.props;
