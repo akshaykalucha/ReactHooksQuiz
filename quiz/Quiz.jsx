18910cd827dda18c851725b665efbe9000fbf920
akshay kalucha <akshaykalucha@gmail.com>
2020-05-27 22:43:35 +0530

    updates

--- quiz/Quiz.jsx
+++ quiz/Quiz.jsx
@@ -8,7 +8,15 @@ import * as actionTypes from '../quiz/Store/actions'
 
 let Nresult = 0
 
-
+function getResult(arr1, arr2) {
+        
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] ===  arr2[i]){
+            Nresult ++
+        }
+    }
+    return Nresult
+}
 
 function Quiz(props) {
 
