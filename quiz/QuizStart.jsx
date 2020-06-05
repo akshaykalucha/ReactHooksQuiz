4ad40fa08040565e4c6301696852a217bdef2507
akshay kalucha <akshaykalucha@gmail.com>
2020-06-05 18:25:35 +0530

    updates

--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -124,14 +124,14 @@ class QuizStart extends Component {
         }
         function chunkArray(myArray, chunk_size){
             var index = 0;
-            // var arrayLength = myArray.length;
-            // var tempArray = [];
+            var arrayLength = myArray.length;
+            var tempArray = [];
             
-            // for (index = 0; index < arrayLength; index += chunk_size) {
-            //     let myChunk = myArray.slice(index, index+chunk_size);
-            //     // Do something if you want with the group
-            //     tempArray.push(myChunk);
-            // }
+            for (index = 0; index < arrayLength; index += chunk_size) {
+                let myChunk = myArray.slice(index, index+chunk_size);
+                // Do something if you want with the group
+                tempArray.push(myChunk);
+            }
         
             return tempArray;
         }
