e611e3542080a544e1a6050ff7d606fd33553fb3
akshay kalucha <akshaykalucha@gmail.com>
2020-06-07 12:40:10 +0530

    updates

--- quiz/QuizStart.jsx
+++ quiz/QuizStart.jsx
@@ -213,12 +213,7 @@ class QuizStart extends Component {
 }
 
 
-function mapStateToProps(state){
-    return {
-        genreReducer: state.Genre,
-        levelReducer: state.level
-    }
-}
+
 
 const mapDispatchToProps = dispatch => {
     return {
