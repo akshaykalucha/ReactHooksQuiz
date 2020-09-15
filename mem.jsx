5b23ddc01cfa6df5fbc5bad36505542b8ae92e85
akshay kalucha <akshaykalucha@gmail.com>
2020-09-16 00:50:20 +0530

    client memory updation

---
+++ mem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import {
+  ApolloClient, ApolloProvider, HttpLink, InMemoryCache
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+ReactDOM.render(
+  <ApolloProvider client={ client }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
